626ac09e370cb6d98c110e1fee10c0e0ff9f08d1
Mahakchhipa <mehekjajpuraom700@gmail.com>
2025-03-22 12:52:34 +0530

    Add  enum

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
+"use strict";
 var a = 11;
 a = "mahak";
+const m = "mahak";
+m = 15;
+var StatusCode;
+(function (StatusCode) {
+    StatusCode["ABANDONED"] = "abandoned status code 500";
+    StatusCode["NOTFOUND"] = "not found status code 404";
+})(StatusCode || (StatusCode = {}));
+StatusCode.NOTFOUND;
--- app.ts
+++ app.ts
@@ -1,2 +1,14 @@
 var a = 11;
 a= "mahak";
+
+
+const m = "mahak";
+m= 15;
+
+
+enum StatusCode{
+    ABANDONED ="abandoned status code 500",
+    NOTFOUND ="not found status code 404",
+}
+
+StatusCode.NOTFOUND
