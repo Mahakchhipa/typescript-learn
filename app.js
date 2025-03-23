aded28d2cb57484f1f1e132003f79b096ab2754b
Mahakchhipa <mehekjajpuraom700@gmail.com>
2025-03-23 20:56:39 +0530

    Add index file

--- app.js
+++ app.js
@@ -9,3 +9,18 @@ var StatusCode;
     StatusCode["NOTFOUND"] = "not found status code 404";
 })(StatusCode || (StatusCode = {}));
 StatusCode.NOTFOUND;
+function abcd(obj) {
+}
+abcd({ name: "mahak", email: "mehekek@gamil.com", password: "123Wm2ekw", gender: "female" });
+function abcde(obj) {
+}
+abcde("mahak");
+class Device {
+    constructor() {
+        this.name = "lg";
+        this.price = "120000";
+        this.catagery = "digital";
+    }
+}
+let a1 = new Device();
+let a2 = new Device();
--- app.ts
+++ app.ts
@@ -11,4 +11,39 @@ enum StatusCode{
     NOTFOUND ="not found status code 404",
 }
 
-StatusCode.NOTFOUND
+StatusCode.NOTFOUND  
+
+interface User {
+    name:string;
+    email:string;
+    password:string;
+    gender?: string; 
+    // optinal ya to feel kro yeh ya nhi ? se defin ekr skhte h yeh 
+}
+
+function abcd(obj:User){
+
+}
+
+abcd({name:"mahak",email:"mehekek@gamil.com",password:"123Wm2ekw" ,gender:"female" })
+
+
+// type aliases
+
+type value = string| null| number | boolean;
+
+function abcde(obj:value){
+
+}
+abcde("mahak")
+
+class Device {
+    name ="lg";
+    price ="120000";
+    catagery ="digital";
+
+
+}
+
+let a1 = new Device();
+let a2 = new Device();
